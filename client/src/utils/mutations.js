76ef0f2300a32d213375d395a71e0071a1805b26
Shinichi <miyakawashi@gmail.com>
2022-04-19 21:33:33 -0400

    frontend removeBooks mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -39,5 +39,14 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-
+   mutation removeBook($bookId: ID!) {
+       removeBook(bookId: $bookId) {
+           _id
+           username
+           savedBooks {
+               authors
+               title
+           }
+       }
+   }
 `;
